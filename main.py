6878ee723fbd21738794f17ca26e3d4f66e1299a
Luckman Qasim <luckmanqasim@gmail.com>
2023-09-08 01:42:42 -0230

    Update: updated code and added print statements to debug

--- main.py
+++ main.py
@@ -13,12 +13,16 @@ if __name__ == "__main__":
     imap_server = os.getenv('INBOX_IMAP_SERVER')
     keywords = os.getenv('KEYWORDS')
 
-    email_filter = EmailFilter(email_address, password, imap_server, keywords)
+    email_filter = EmailFilter(email_address, password, imap_server, keywords, destination_folder='Test')
     
     try:
         email_filter.connect()
+        print('connected')
         email_ids = email_filter.matching_email_ids()
-        email_filter.fetch_emails(email_ids)
+        print(email_ids)
+        email_filter.create_destination_folder()
+        print('labels')
+        email_filter.move_emails(email_ids)
     except Exception as e:
         print(f"An error occurred: {str(e)}")
     finally:
