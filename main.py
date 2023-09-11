9f9e8040543985d68c1ad06bb53826f8755e0d4c
Luckman Qasim <luckmanqasim@gmail.com>
2023-09-10 23:25:43 -0230

    Update: loaded message sender environmental variables and added send email test cases

--- main.py
+++ main.py
@@ -12,11 +12,16 @@ if __name__ == "__main__":
     email_address = os.getenv('INBOX_ADDRESS')
     password = os.getenv('INBOX_PASSWORD')
     imap_server = os.getenv('INBOX_IMAP_SERVER')
-    #keywords = os.getenv('KEYWORDS')
     keywords = ['job']
+
+    sender_email = os.getenv('SENDER_ADDRESS')
+    sender_password = os.getenv('SENDER_PASSWORD')
+    smtp_server = os.getenv('SMTP_SERVER')
+    smtp_port = os.getenv('SMTP_PORT')
     
 
     email_filter = EmailFilter(email_address, password, imap_server, keywords, destination_folder='Test')
+    msg_client = Message(smtp_server, smtp_port, sender_email, sender_password)
     
     try:
         email_filter.connect()
@@ -26,7 +31,8 @@ if __name__ == "__main__":
         email_filter.create_destination_folder()
         print('labels')
         email_filter.move_emails(email_ids)
-        email_filter.fetch_emails(email_ids)
+        new_datas = email_filter.fetch_emails(email_ids)
+        msg_client.send_email(receiver_email='7097666866@msg.koodomobile.com', new_data=new_datas)
     except Exception as e:
         print(f"An error occurred: {str(e)}")
     finally:
