b1ba6afde000054b74ba3098eff674542dde0f19
Luckman Qasim <luckmanqasim@gmail.com>
2023-09-12 02:50:01 -0230

    added test case main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+from filter_email import EmailFilter
+from dotenv import load_dotenv
+import os
+from message_client import Message
+
+if __name__ == "__main__":
+
+    # access evironmental variables
+    load_dotenv()
+
+    email_address = os.getenv('INBOX_ADDRESS')
+    password = os.getenv('INBOX_PASSWORD')
+    imap_server = os.getenv('INBOX_IMAP_SERVER')
+    keywords = ['job']
+
+    sender_email = os.getenv('SENDER_ADDRESS')
+    sender_password = os.getenv('SENDER_PASSWORD')
+    smtp_server = os.getenv('SMTP_SERVER')
+    smtp_port = os.getenv('SMTP_PORT')
+
+    receiver = os.getenv('RECEIVER')
+    
+    # create email filter and a message client for testing
+    email_filter = EmailFilter(email_address, password, imap_server, keywords, destination_folder='Test')
+    
+    msg_client = Message(smtp_server, smtp_port, sender_email, sender_password)
+    
+
+    try:
+        email_filter.connect()
+        print('connected')
+        email_ids = email_filter.matching_email_ids()
+        print(email_ids)
+        email_filter.create_destination_folder()
+        print('labels')
+        email_filter.move_emails(email_ids)
+        new_datas = email_filter.fetch_emails(email_ids)
+        msg_client.send_email(receiver_email=receiver, new_data=new_datas)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+    finally:
+        email_filter.disconnect()
